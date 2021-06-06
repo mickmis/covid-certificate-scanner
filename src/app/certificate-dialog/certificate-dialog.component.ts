4af89cc6333cd4ea6ce8be297849b5af2f7fc484
Mickaël Misbach <mickael@misba.ch>
2021-06-06 15:38:35 +0200

    update display of certificate data

--- src/app/certificate-dialog/certificate-dialog.component.ts
+++ src/app/certificate-dialog/certificate-dialog.component.ts
@@ -5,6 +5,7 @@ import {DigitalCovidCertificate} from '../digital-covid-certificate';
 import {EuDigitalCovidCert121} from '../models/eu-digital-covid-cert/eu-digital-covid-cert-121';
 import {toHexString, formatDate, timestampToDate} from '../utils';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {ValueSetsService, ValueSet} from '../value-sets.service';
 
 @Component({
   selector: 'app-certificate-dialog',
@@ -16,8 +17,10 @@ export class CertificateDialogComponent implements OnInit {
   toHexString = toHexString;
   formatDate = formatDate;
   timestampToDate = timestampToDate;
+  ValueSet = ValueSet;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public _cert: Observable<DigitalCovidCertificate>) { }
+  constructor(@Inject(MAT_DIALOG_DATA) private _cert: Observable<DigitalCovidCertificate>,
+              private valueSetsService: ValueSetsService) { }
 
   ngOnInit(): void { }
 
@@ -33,16 +36,16 @@ export class CertificateDialogComponent implements OnInit {
     return this.cert.pipe(
       map(cert => cert.certificate.nam),
       map(nam => {
-      if (nam.fn && nam.gn) {
-        return `${nam.fn}, ${nam.gn}`;
-      } else if (nam.fn && !nam.gn) {
-        return `${nam.fn}`;
-      } else if (!nam.fn && nam.gn) {
-        return `${nam.gn}`;
-      } else {
-        return '[missing]';
-      }
-    }));
+        if (nam.fn && nam.gn) {
+          return `${nam.fn}, ${nam.gn}`;
+        } else if (nam.fn && !nam.gn) {
+          return `${nam.fn}`;
+        } else if (!nam.fn && nam.gn) {
+          return `${nam.gn}`;
+        } else {
+          return '[missing]';
+        }
+      }));
   }
 
   get transliteratedNames(): Observable<string> {
@@ -50,5 +53,9 @@ export class CertificateDialogComponent implements OnInit {
       map(cert => cert.certificate.nam),
       map(nam => `${nam.fnt}${nam.gnt ? ', ' + nam.gnt : ''}`)
     );
+  }
+
+  getValueName(valueSet: ValueSet, valueCode: string): string {
+    return `${this.valueSetsService.getValueName(valueSet, valueCode)} (${valueCode})`;
   }
 }
